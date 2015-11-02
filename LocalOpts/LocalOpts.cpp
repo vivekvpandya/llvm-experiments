2b4beb7382f59a4759028965d6c70bcccf0d9418
Vivek Pandya <vivekvpandya@gmail.com>
2015-11-02 22:02:45 +0530

    Little progress for Constant Folding

--- LocalOpts/LocalOpts.cpp
+++ LocalOpts/LocalOpts.cpp
@@ -6,6 +6,7 @@
 #include "llvm/Support/Casting.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/Constants.h"
 
 #include <ostream>
 #include <fstream>
@@ -18,41 +19,54 @@ namespace {
 class LocalOpts : public BasicBlockPass {
 
 public:
-	static char ID;
-	LocalOpts() : BasicBlockPass(ID){ }
-	~LocalOpts() { }
+  static char ID;
+  LocalOpts() : BasicBlockPass(ID){ }
+  ~LocalOpts() { }
 
-	virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+  virtual void getAnalysisUsage(AnalysisUsage &AU) const {
     AU.setPreservesCFG(); // These local optimizations do not  modifiy the CFG
   }
 
   virtual bool runOnBasicBlock(BasicBlock &BB)
   {
-  	for(BasicBlock::iterator i = BB.begin(), e = BB.end(); i != e ; i++)
-  	{
-  		if( i->isBinaryOp() )
-  		{
-  			switch ( i->getOpcode() )
-  			{
-  				case Instruction::Add:
-  					outs() << i->getOperand(0)->getName() << " " << i->getOperand(1)->getName();
-  				break;
-  				case Instruction::Sub:
-  					outs() << i->getOperand(0)->getName() << " " << i->getOperand(1)->getName();
-  				break;
-  				case Instruction::Mul:
-  					outs() << i->getOperand(0)->getName() << " " << i->getOperand(1)->getName();
-  				break;
+    for(BasicBlock::iterator i = BB.begin(), e = BB.end(); i != e ; i++)
+    {
+      if( i->isBinaryOp() )
+      {
+        Value *firsOperand = i->getOperand(0);
+        if(ConstantInt *cone = dyn_cast<ConstantInt>(firsOperand))
+        {  
+          APInt coneval = cone->getValue();
+          Value *secondOperand = i->getOperand(1);
+          if(ConstantInt *ctwo = dyn_cast<ConstantInt>(secondOperand))
+          { APInt ctwoval = ctwo->getValue();
+            i->dump();
+            outs() << "\n";
+            APInt eval ;
+            switch(i->getOpcode()){
+              case Instruction::Add :
+              eval = coneval + ctwoval;
+              break;
+              case Instruction::Sub :
+              eval = coneval - ctwoval;
+              break;
+              case Instruction::Mul :
+              eval = coneval * ctwoval;
+              break;
+            }
+            outs() << eval << "\n";
 
-  			}
-  		}
-  	}
-  	return false;
+
+          }
+        }
+      }
+    }
+    return false;
   }
 
   virtual bool runOnFunction(Function &F)
   {
-  	for (Function::iterator f = F.begin(), fe = F.end(); f != fe; ++fe) {
+    for (Function::iterator f = F.begin(), fe = F.end(); f != fe; ++fe) {
       runOnBasicBlock(*f);
     }
     return false;
